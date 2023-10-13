f0879f73f042174e93b462e9e2eb7e5a6c798a21
{Hongkyoungseuk} <tug234@daum.net>
2023-10-13 14:05:34 +0900

    10.13 14:05

--- src/IOStrings.java
+++ src/IOStrings.java
@@ -1,14 +1,23 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Scanner;
 
 public class IOStrings {
     
     public String[] loandArray(InputStream is, int n) {
-        
-        return ;
+        //is에 의해 지정된 입력 채널에서 읽은 n개의 문자열 배열을 반환합니다.
+        String[] st = new String[n];
+        for (int i = 0; i < n ; i++) {
+            st[i] = is.toString();
+        }
+        return st;
     }
 
     public void saveArray(OutputStream os, String[] sa) {
+        for (int i = 0 ; i < sa.length ; i++) {
+            System.out.println();
+        }
+        
         
     }
 }
