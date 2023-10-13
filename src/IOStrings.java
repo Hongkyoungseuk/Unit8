e023d4322bc44517a216ed5822e2851651429ab6
{Hongkyoungseuk} <tug234@daum.net>
2023-10-13 09:49:14 +0900

    first commit

---
+++ src/IOStrings.java
@@ -0,0 +1,14 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class IOStrings {
+    
+    public String[] loandArray(InputStream is, int n) {
+        
+        return ;
+    }
+
+    public void saveArray(OutputStream os, String[] sa) {
+        
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+    // Exercise_1
+        // 정적 메서드 작성 (IOStrings 클래스 작성)
+    // Exercise_2
+        // 값이 0이 삽입될때까지 키보드에서 양의 정수 시퀀스를 읽고 읽은 값의 평균을 나타내는 double을 반화하는 공개 정적 방법을 작성하세요.
+    // Exercise_3
+
+    // Exercise_4
+
+    // Exercise_5
+
+    // Exercise_6
+
+    // Exercise_7
+
+    // Exercise_8
+
+    // Exercise_9
+        
+    }
+}
