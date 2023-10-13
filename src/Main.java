024098b0154b5e7a2811a974943a0855a319eb31
{Hongkyoungseuk} <tug234@daum.net>
2023-10-13 18:20:29 +0900

    10.13 18:20

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    // Exercise_1
+    // Exercise_1 TODO 12%
         // 정적 메서드 작성 (IOStrings 클래스 작성)
-    // Exercise_2
+    // Exercise_2 
         // 값이 0이 삽입될때까지 키보드에서 양의 정수 시퀀스를 읽고 읽은 값의 평균을 나타내는 double을 반화하는 공개 정적 방법을 작성하세요.
-    // Exercise_3
+    // Exercise_3 TODO 0%
+        // '/',';','%' 문자중 하나로 시작하는 파일의 모든 줄을 비디오에 인쇄하는 공개 정적 방법을 작성하십시오. 파일의 이름은 매개 변수로 지정되어야 합니다.
+    // Exercise_4 
+        // 파일 이름과 정수n을 나타내는 문자열을 매개변수로 취하고 파일에 크기 n의 곱샘 테이블을 쓰는 공개 정적 메서드를 작성하십시오. 
+    // Exercise_5 TODO
+        // 구성된 학생들이 얻은 시험 점수에 대한 파일 정보를 읽는 공개 정적 방법을 작성하십시오.
+    // Exercise_6 TODO
 
-    // Exercise_4
+    // Exercise_7 TODO
 
-    // Exercise_5
+    // Exercise_8 TODO
 
-    // Exercise_6
-
-    // Exercise_7
-
-    // Exercise_8
-
-    // Exercise_9
+    // Exercise_9 TODO
         
     }
 }
